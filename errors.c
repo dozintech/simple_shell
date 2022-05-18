d17e81519533c716cd330aa2de5647b0ed2d2268
dozonalx <100067688+dozonalx@users.noreply.github.com>
2022-05-18 16:16:56 +0100

    Update errors.c

--- errors.c
+++ errors.c
@@ -3,7 +3,7 @@
 /**
  * print_error - prints error messages to standard error
  * @vars: pointer to struct of variables
- * @msg: message to print
+ * @msg: message to print out
  *
  * Return: void
  */
